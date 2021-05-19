03b8686ec031faffdbc4891ede2da0b98857037b
vitoriaS <34271258+VitoriaSandri@users.noreply.github.com>
2021-05-19 20:39:49 -0300

    DashboardClient

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import TriageClient from '../pages/TriageClient';
 
 const App = createStackNavigator();
 
@@ -10,6 +11,7 @@ const AppRoutes: React.FC = () => (
     screenOptions={{ headerShown: false}}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="TriageClient" component={TriageClient} />
   </App.Navigator>
 );
 
